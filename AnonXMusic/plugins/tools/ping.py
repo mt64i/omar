90729192269dcc8ee30194e383fc2b7629f7768a
Dev.Emma <139269208+bbnnQ@users.noreply.github.com>
2023-08-21 16:26:04 +0300

    Update ping.py

--- AnonXMusic/plugins/tools/ping.py
+++ AnonXMusic/plugins/tools/ping.py
@@ -4,7 +4,6 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from AnonXMusic import app
-from strings.filters import command
 from AnonXMusic.core.call import Anony
 from AnonXMusic.utils import bot_sys_stats
 from AnonXMusic.utils.decorators.language import language
@@ -12,7 +11,7 @@ from AnonXMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
-@app.on_message(command(["/ping", "/alive", "بنغ"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
