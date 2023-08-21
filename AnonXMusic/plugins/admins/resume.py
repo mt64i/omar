8dd91f9bc5af7a6b96c56d243bb1070fb7adad37
Dev.Emma <139269208+bbnnQ@users.noreply.github.com>
2023-08-22 01:09:47 +0300

    Update resume.py

--- AnonXMusic/plugins/admins/resume.py
+++ AnonXMusic/plugins/admins/resume.py
@@ -2,6 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from AnonXMusic import app
+from strings.filters import command
 from AnonXMusic.core.call import Anony
 from AnonXMusic.utils.database import is_music_playing, music_on
 from AnonXMusic.utils.decorators import AdminRightsCheck
@@ -9,7 +10,7 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
+@app.on_message(command(["/resume", "/cresume", "تكميل"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
