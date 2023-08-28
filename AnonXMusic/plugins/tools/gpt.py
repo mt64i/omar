3bff879b5c6dd9f5ab0fd7a1a4643b750ec15801
Dev.Emma <139269208+bbnnQ@users.noreply.github.com>
2023-08-29 01:55:09 +0300

    Update gpt.py

--- AnonXMusic/plugins/tools/gpt.py
+++ AnonXMusic/plugins/tools/gpt.py
@@ -41,4 +41,4 @@ def reply_gpt(client, message:Message):
         message_id = message.reply_to_message.id
     else:
         message_id = None
-    await message.reply_text(chat_id=chat_id, text=reply_text, reply_to_message_id=message_id)
+    client.replay_message(chat_id=chat_id, text=reply_text, reply_to_message_id=message_id)
