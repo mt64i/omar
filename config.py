6edbb89d000531e30e9b73cfaa75d47f4c73c752
Dev.Emma <139269208+bbnnQ@users.noreply.github.com>
2023-09-07 18:54:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bbnnQ/MusicV2",
+    "https://github.com/bbnnQ/Emma2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
@@ -43,7 +43,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/cczza")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/cczza")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT","True"))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
