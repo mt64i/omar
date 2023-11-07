918b90c38f87b29543455e30aa0dd915d73f7ef5
coowl <129096669+coowl@users.noreply.github.com>
2023-11-07 12:52:35 +0400

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -1,7 +1,7 @@
 import random
 import string
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
+from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message, InlineKeyboardButton
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
