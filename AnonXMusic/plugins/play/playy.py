c0ca5043c2a50a6c4acfabac394daea5ec5a86f3
coowl <129096669+coowl@users.noreply.github.com>
2023-11-08 01:19:29 +0400

    Update playy.py

--- AnonXMusic/plugins/play/playy.py
+++ AnonXMusic/plugins/play/playy.py
@@ -2,30 +2,31 @@ from  pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import random
 import string
 from ast import ExceptHandler
-from pyrogram import filters, Client
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, Message
+
+from pyrogram import filters
+from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
+                            Message)
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import BOT_TOKEN
+from config import BANNED_USERS, lyrical
+from strings import get_command
 from strings.filters import command
-from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Anony
+from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
+                        YouTube, app)
+from AnonXMusic.core.call import Yukki
 from AnonXMusic.utils import seconds_to_min, time_to_seconds
 from AnonXMusic.utils.channelplay import get_channeplayCB
+from AnonXMusic.utils.database import is_video_allowed
 from AnonXMusic.utils.decorators.language import languageCB
 from AnonXMusic.utils.decorators.play import PlayWrapper
 from AnonXMusic.utils.formatters import formats
-from AnonXMusic.utils.inline import (
-    botplaylist_markup,
-    livestream_markup,
-    playlist_markup,
-    slider_markup,
-    track_markup,
-)
+from AnonXMusic.utils.inline.play import (livestream_markup,
+                                          playlist_markup,
+                                          slider_markup, track_markup)
+from AnonXMusic.utils.inline.playlist import botplaylist_markup
 from AnonXMusic.utils.logger import play_logs
 from AnonXMusic.utils.stream.stream import stream
-from config import BANNED_USERS, lyrical
 
 
 @app.on_message(command(["شغل","تشغيل"])
