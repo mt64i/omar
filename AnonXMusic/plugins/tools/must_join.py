43f543ffc1c908de210615f1fdcb413fd7db11b1
coowl <129096669+coowl@users.noreply.github.com>
2023-11-07 13:03:14 +0400

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -5,27 +5,27 @@ from AnonXMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/Z_c_K":  # Not compulsory
+    if not "https://t.me/zzsvv":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("Z_c_K", msg.from_user.id)
+            await bot.get_chat_member("zzsvv", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/Z_c_K".isalpha():
-                link = "https://t.me/Z_c_K"
+            if "https://t.me/zzsvv".isalpha():
+                link = "https://t.me/zzsvv"
             else:
-                chat_info = await bot.get_chat("Z_c_K")
+                chat_info = await bot.get_chat("zzsvv")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @Z_c_K .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @zzsvv .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("اصاله نصري | Assala nasri", url=link)]
+                        [InlineKeyboardButton("برود .", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @Z_c_K !")
+        print(f"I'm not admin in the MUST_JOIN chat @zzsvv !")
