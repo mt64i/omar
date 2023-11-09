a42b83c4e51c26a9f31ea720d98c131eb2bc9efd
Abdulhadi <129096669+coowl@users.noreply.github.com>
2023-11-09 06:03:46 +0400

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -21,7 +21,7 @@ async def must_join_channel(bot: Client, msg: Message):
                     f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @zzsvv .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("برود .", url=link)]
+                        [InlineKeyboardButton("اضغط للأشتراك .", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
