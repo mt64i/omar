8cb733650d686799128c432b3c959b21ac61ffa8
Abdulhadi <129096669+coowl@users.noreply.github.com>
2023-11-09 16:31:08 +0400

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -45,7 +45,7 @@ async def check_is_joined(message):
         return False
 
 
-@app.on_message(command(["تشغيل"])
+@app.on_message(command(["شغل","تشغيل"])
     & filters.group
     & ~BANNED_USERS
 )
