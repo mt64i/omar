532998a6de8268594a6ad47e3a91f34589e1af23
Abdulhadi <129096669+coowl@users.noreply.github.com>
2023-11-15 11:11:30 +0400

    Update stop.py

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -25,3 +25,20 @@ async def stop_music(cli, message: Message, _, chat_id):
     await message.reply_text(
         _["admin_5"].format((message.from_user.mention if message.from_user else message.chat.title)), reply_markup=close_markup(_)
     )
+
+
+
+@app.on_message(
+    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
+)
+@app.on_message(
+    command(["انهاء","ايقاف"]) & filters.channel & ~BANNED_USERS
+)
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await Anony.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format((message.from_user.mention if message.from_user else message.chat.title)), reply_markup=close_markup(_)
+    )
