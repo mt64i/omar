693937442f57d3111a48ec02cda67f56cf9dba2c
Abdulhadi <129096669+coowl@users.noreply.github.com>
2023-12-26 14:27:38 +0400

    Update abdulhadi.py

--- AnonXMusic/plugins/tools/abdulhadi.py
+++ AnonXMusic/plugins/tools/abdulhadi.py
@@ -1,6 +1,5 @@
 import asyncio
 from pyrogram import Client, filters
-from strings import get_command
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
