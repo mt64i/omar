6f8f4c235418b0e8b1f32bc02da11e46e81bcd7c
Abdulhadi <129096669+coowl@users.noreply.github.com>
2023-12-28 19:04:00 +0400

    Add files via upload

--- strings/filters.py
+++ strings/filters.py
@@ -3,11 +3,7 @@ from typing import List, Union
 from pyrogram import filters
 
 
-other_filters = filters.group  & ~filters.via_bot & ~filters.forwarded
-other_filters2 = (
-    filters.private  & ~filters.via_bot & ~filters.forwarded
-)
-
+other_filters = filters.group
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, "")
