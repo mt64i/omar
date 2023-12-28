fe1ea7f5f39026b3dfbfe318c053eaa6f38620cb
Abdulhadi <129096669+coowl@users.noreply.github.com>
2023-12-28 14:32:07 +0400

    Update filters.py

--- strings/filters.py
+++ strings/filters.py
@@ -3,7 +3,11 @@ from typing import List, Union
 from pyrogram import filters
 
 
-other_filters = filters.group
+other_filters = filters.group  & ~filters.via_bot & ~filters.forwarded
+other_filters2 = (
+    filters.private  & ~filters.via_bot & ~filters.forwarded
+)
+
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, "")
