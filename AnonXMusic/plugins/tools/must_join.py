ba1b79e759a9ea17f8cbe589b629cdc5eb50dd67
سورس الاعصار <118974844+BLAKAQ@users.noreply.github.com>
2023-12-30 14:25:32 +0400

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -5,20 +5,20 @@ from AnonXMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/zzsvv":  # Not compulsory
+    if not "https://t.me/A1DIIU":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("zzsvv", msg.from_user.id)
+            await bot.get_chat_member("A1DIIU", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/zzsvv".isalpha():
-                link = "https://t.me/zzsvv"
+            if "https://t.me/A1DIIU".isalpha():
+                link = "https://t.me/A1DIIU"
             else:
-                chat_info = await bot.get_chat("zzsvv")
+                chat_info = await bot.get_chat("A1DIIU")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @zzsvv .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @A1DIIU .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("اضغط للأشتراك .", url=link)]
@@ -28,4 +28,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @zzsvv !")
+        print(f"I'm not admin in the MUST_JOIN chat @A1DIIU !")
