63af1b8a8626f1c1bfa444e20e03fb4699908b21
سورس الاعصار <118974844+BLAKAQ@users.noreply.github.com>
2023-12-30 15:52:53 +0400

    Update sudoers.py

--- AnonXMusic/plugins/sudo/sudoers.py
+++ AnonXMusic/plugins/sudo/sudoers.py
@@ -45,7 +45,7 @@ async def userdel(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
-@app.on_message(command(["المطورين"]) & ~BANNED_USERS)
+@app.on_message(command(["/sudolist", "المطورين"]) & ~BANNED_USERS)
 @language
 async def sudoers_list(client, message: Message, _):
     text = _["sudo_5"]
