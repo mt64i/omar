e9538cbbce713127665afdca3216730225e2d4b2
Omar Al khalil <161373973+mt64i@users.noreply.github.com>
2024-02-27 22:44:36 +0100

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,13 +51,13 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("A1DIIU")
-                await self.one.join_chat("A1DIIU")
+                await self.one.join_chat("E_A_1L")
+                await self.one.join_chat("E_A_1L")
             except:
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.one.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -72,13 +72,13 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("A1DIIU")
-                await self.two.join_chat("A1DIIU")
+                await self.two.join_chat("E_A_1L")
+                await self.two.join_chat("E_A_1L")
             except:
                 pass
             assistants.append(2)
             try:
-                await self.two.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.two.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -93,13 +93,13 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("A1DIIU")
-                await self.three.join_chat("A1DIIU")
+                await self.three.join_chat("E_A_1L")
+                await self.three.join_chat("E_A_1L")
             except:
                 pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.three.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -114,13 +114,13 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("A1DIIU")
-                await self.four.join_chat("A1DIIU")
+                await self.four.join_chat("E_A_1L")
+                await self.four.join_chat("E_A_1L")
             except:
                 pass
             assistants.append(4)
             try:
-                await self.four.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.four.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -135,13 +135,13 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("A1DIIU")
-                await self.five.join_chat("A1DIIU")
+                await self.five.join_chat("E_A_1L")
+                await self.five.join_chat("E_A_1L")
             except:
                 pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.five.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
