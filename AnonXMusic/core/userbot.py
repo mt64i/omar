7c310c85584359f5c2e346ec471257788acaff68
Omar Al khalil <161373973+mt64i@users.noreply.github.com>
2024-02-28 00:45:28 +0300

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -57,7 +57,7 @@ class Userbot(Client):
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
+                await self.one.send_message(config.LOGGER_ID, "1تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -78,7 +78,7 @@ class Userbot(Client):
                 pass
             assistants.append(2)
             try:
-                await self.two.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
+                await self.two.send_message(config.LOGGER_ID, "2تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -99,7 +99,7 @@ class Userbot(Client):
                 pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
+                await self.three.send_message(config.LOGGER_ID, "3تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -120,7 +120,7 @@ class Userbot(Client):
                 pass
             assistants.append(4)
             try:
-                await self.four.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
+                await self.four.send_message(config.LOGGER_ID, "4تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -141,7 +141,7 @@ class Userbot(Client):
                 pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد")
+                await self.five.send_message(config.LOGGER_ID, "5تم تشغيل الحساب المساعد")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
