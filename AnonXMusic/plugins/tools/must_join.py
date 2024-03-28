4325c5872d315ce385291286fa29295df7f9401a
Omar Al khalil <161373973+mt64i@users.noreply.github.com>
2024-03-28 19:50:49 +0100

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -5,20 +5,20 @@ from AnonXMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/E_A_1L":  # Not compulsory
+    if not "https://t.me/syrea_surs":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("E_A_1L", msg.from_user.id)
+            await bot.get_chat_member("syrea_surs", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/E_A_1L".isalpha():
-                link = "https://t.me/E_A_1L"
+            if "https://t.me/syrea_surs".isalpha():
+                link = "https://t.me/syrea_surs"
             else:
-                chat_info = await bot.get_chat("E_A_1L")
+                chat_info = await bot.get_chat("syrea_surs")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @E_A_1L .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @syrea_surs .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("اضغط للأشتراك .", url=link)]
@@ -28,4 +28,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @E_A_1L !")
+        print(f"I'm not admin in the MUST_JOIN chat @syrea_surs !")
